38bb4b1c678b0203409d93656619f3954cffe03d
Kyle Butler <232253772+initcommit43@users.noreply.github.com>
2026-03-31 10:13:23 +0200

    initial spring boot setup

---
+++ gameshelf/src/main/java/com/gameshelf/GameshelfApplication.java
@@ -0,0 +1,13 @@
+package com.gameshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameshelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameshelfApplication.class, args);
+	}
+
+}
